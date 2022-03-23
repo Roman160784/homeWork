66af45ec87b80f143333367fc5e82b9a11b7747b
Roman160784 <matveyenkaraman@gmail.com>
2022-03-23 15:57:14 +0300

    add action creater

--- src/p2-homeworks/h13/api/redux/requestReducer.ts
+++ src/p2-homeworks/h13/api/redux/requestReducer.ts
@@ -11,7 +11,22 @@ export const initialState :initialStateType = {
 
 
 export const requestReducers = (state: initialStateType = initialState, action: MainActionType) => {
+    switch(action.type){
+        case 'REQUEST/SET-CHECKBOX-VALUE' : {
+            return {...state, success: action.success}
+        }
+    }
+
     return state
 }
 
-export type MainActionType = ''
+export type MainActionType = setCheckBoxValueType
+
+export type setCheckBoxValueType = ReturnType<typeof setCheckBoxValueAC>
+
+
+export const setCheckBoxValueAC = (success: boolean) => ({type : 'REQUEST/SET-CHECKBOX-VALUE', success} as const)
+
+export const setCheckBoxValueTC = (success: boolean) => {
+
+}
