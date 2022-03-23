98b2a6cf0b44cce98b025d9e22c321348e57c1df
Roman160784 <matveyenkaraman@gmail.com>
2022-03-23 16:01:46 +0300

    cteated API

--- src/p2-homeworks/h13/api/requestAPI.ts
+++ src/p2-homeworks/h13/api/requestAPI.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL : 'https://neko-cafe-back.herokuapp.com/auth/test'
+    baseURL : 'https://neko-cafe-back.herokuapp.com/'
 })
+
+
+export const requestAPI = {
+    postSuccess(success: boolean) {
+        instance.post('auth/test', {success})
+    }
+}
