d11db932df490095fda3fe91fa36431eb5271cb2
Roman160784 <matveyenkaraman@gmail.com>
2022-03-24 11:24:13 +0300

    refactor

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,4 +1,6 @@
 import React, { ChangeEvent, useState } from "react";
+import { useSelector } from "react-redux";
+import { AppStoreType } from "../h10/bll/store";
 
 type RequestPropsType = {
     getCheckBoxValue: (success: boolean) => void
@@ -7,6 +9,9 @@ type RequestPropsType = {
 
 export const  Request = ({getCheckBoxValue, ...props}: RequestPropsType) => {
 
+    const message = useSelector<AppStoreType, string | null >(state => state.request.success)
+    
+    
     const [checkboxValue, setCheckboxValue] = useState<boolean>(false)
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,7 +28,7 @@ export const  Request = ({getCheckBoxValue, ...props}: RequestPropsType) => {
         <div>
             <button onClick={onClickHandler}>Click</button>
             <input type="checkbox" onChange={onChangeHandler}/>
-            
+            {message !== null ? <span>{message}</span> : null}
         </div>
     )
 }
